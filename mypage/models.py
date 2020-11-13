cd92a3ecd06a142ffe1d5db0f671b069dc0f3f68
winterash2 <winterash2@naver.com>
2020-11-13 12:44:57 +0900

    자잘한 오류 수정

--- mypage/models.py
+++ mypage/models.py
@@ -1,7 +1,17 @@
-from django.db import models
+# from django.db import models
+# from recipe.models import Recipe
+# from django.contrib.auth.models import User
 
-# Create your models here.
+# # Create your models here.
+
+# class ExperienceRecipe(models.Model):
+#     recipe = models.ManyToManyField(Recipe)
+#     created_date = models.DateTimeField()
+#     user_detail = models.ForeignKey(UserDetail)
 
 
-class UserDetail(models.Model):
-    expirence_recipes
+# class UserDetail(models.Model):
+#     user = models.ForeignKey(User)
+#     wish_recipes = models.ManyToManyField(Recipe)
+#     total_time = models.IntegerField()
+    
